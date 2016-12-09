a3cf26824d5b365890b3f99e8718306d10df83d2
mattericbrown <matt.eric.brown@gmail.com>
2016-12-08 22:55:54 -0800

    basic react component app that takes name as an input and renders it to page

---
+++ HelloReact/public/app.jsx
@@ -0,0 +1,77 @@
+var GreeterMessage = React.createClass({
+  render: function () {
+    return (
+        <div>
+          <h1>Some H1 </h1>
+          <p> Some message </p>
+        </div>
+    );
+  }
+});
+
+var GreeterForm = React.createClass({
+  render: function () {
+    return (
+      <form>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    )
+  }
+});
+
+var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React',
+      message: 'this is a test!'
+    };
+  },
+  getInitialState: function () {
+    return {
+      name: this.props.name
+    }
+  },
+  onButtonClick: function (e) {
+    e.preventDefault();
+
+    var nameRef = this.refs.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+
+    if (typeof name === 'string' && name.length > 0){
+
+      this.setState({
+        name: name
+      });
+    }
+  },
+  render: function () {
+    var name = this.state.name;
+    var message = this.props.message;
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p> {message} </p>
+
+        <GreeterMessage/>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+
+        <GreeterForm/>
+
+      </div>
+    );
+  }
+});
+
+var firstName = "Matt";
+var message = 'not default!'
+
+ReactDOM.render(
+  <Greeter name={firstName} message={message}/>,
+  document.getElementById('app')
+);
